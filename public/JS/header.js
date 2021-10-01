402267be62f51e02fc5a707d2d800796bf1b7a18
Hyeongyun <shg7271@gmail.com>
2021-10-02 00:25:09 +0900

    Update header: max-w 1023px responsive

--- public/JS/header.js
+++ public/JS/header.js
@@ -3,7 +3,7 @@
   <img src="../img/LIO_Link_In_One.svg" alt="" class="header__logo" />
   <ul class="nav">
     <li class="nav__item"><a href="" class="nav__link">서비스소개</a></li>
-    <li class="nav__item"><a href="" class="nav__link">스터디</a></li>
+    <li class="nav__item"><a href="../HTML/study-find.html" class="nav__link">스터디</a></li>
     <li class="nav__item"><a href="" class="nav__link">프로젝트</a></li>
     <li class="nav__item"><a href="" class="nav__link">게시판</a></li>
     <li class="nav__item"><a href="" class="nav__link">최근트렌드</a></li>
