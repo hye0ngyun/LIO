bcff2d9b128719f71c8d6635a328b0af6cfa97bc
Hyeongyun <shg7271@gmail.com>
2021-10-07 23:18:29 +0900

    Update style: modal, whole font

--- public/JS/login-modal.js
+++ public/JS/login-modal.js
@@ -20,10 +20,14 @@
         class="modal-content__input"
       />
       <div class="modal-content_ly1">
-        <a href="" class="modal-content__box modal-content__box_small"
+        <a
+          href="#login-modal-find"
+          class="modal-content__box modal-content__box_small"
           >비밀번호 찾기</a
         >
-        <a href="" class="modal-content__box modal-content__box_small"
+        <a
+          href="#login-modal-signup"
+          class="modal-content__box modal-content__box_small"
           >회원가입</a
         >
       </div>
@@ -32,12 +36,73 @@
         class="modal-content__box modal-content__box_big"
         value="로그인"
       />
-      <a href="" class="modal-content__box modal-content__box_big"
+      <a href="" class="modal-content__box modal-content__box_big modal-content__google"
         >구글로 로그인</a
       >
-      <a href="" class="modal-content__box modal-content__box_big"
+      <a href="" class="modal-content__box modal-content__box_big modal-content__naver"
         >네이버로 로그인</a
       >
+    </div>
+  </div>
+  <div class="login-modal" id="login-modal-find">
+    <div class="login-modal__content modal-content">
+      <a href="#open-login-modal">
+        <img
+          class="modal-content__prev"
+          src="../img/keyboard_arrow_left_white_24dp.svg"
+          alt="modal-prev"
+        />
+      </a>
+      <a href="#open-login-modal">
+        <img
+          class="modal-content__close"
+          src="../img/close_white_24dp.svg"
+          alt="modal-close"
+        />
+      </a>
+      <input
+        type="email"
+        placeholder="이메일"
+        class="modal-content__input"
+      />
+      <input
+        type="submit"
+        class="modal-content__box modal-content__box_big"
+        value="비밀번호 초기화"
+      />
+    </div>
+  </div>
+  <div class="login-modal" id="login-modal-signup">
+    <div class="login-modal__content modal-content">
+      <a href="#open-login-modal">
+        <img
+          class="modal-content__prev"
+          src="../img/keyboard_arrow_left_white_24dp.svg"
+          alt="modal-prev"
+        />
+      </a>
+      <a href="#">
+        <img
+          class="modal-content__close"
+          src="../img/close_white_24dp.svg"
+          alt="modal-close"
+        />
+      </a>
+      <input
+        type="email"
+        placeholder="이메일"
+        class="modal-content__input"
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        class="modal-content__input"
+      />
+      <input
+        type="submit"
+        class="modal-content__box modal-content__box_big"
+        value="회원가입"
+      />
     </div>
   </div>
 </form>`
